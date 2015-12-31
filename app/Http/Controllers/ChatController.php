fe496e70f3b9326f317ddb6f479d977da91cb2bf
isojunior <isojocker@gmail.com>
2016-01-01 01:19:29 +0700

    Update Check Session
    Update Check Session

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -10,9 +10,19 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Redirect;
 
 class ChatController extends Controller
 {
+
+	public function __construct()
+	{
+		if(!Session::has('user'))
+		{
+			return Redirect::to('/')->send();
+		}
+	}
+
 	public function getChatView()
 	{
 		return View('chats.main');
