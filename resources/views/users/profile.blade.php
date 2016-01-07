a7c51b19900c8fa313e08630f3e54b66602cf88d
Tuarender <tuarender005@gmail.com>
2016-01-08 00:26:09 +0700

    update profile view

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -7,32 +7,30 @@
                     <div class="loginPanel loginPanelHeader panel-heading">Account</div>
                     <div class="panel-body">
                         @include('partials.flashmessage')
-                        @foreach($profile as $data)
                             <div class="row">
                                 <img class="avatar" src="img/avatar.png" alt="avatar" />
                             </div>
                             <div class="row">
                                 <div class="form-group">
-                                    <label class="col-sm-4 control-label">{{ $data['FIRST_NAME'] }}</label>
+                                    <label class="col-sm-4 control-label">{{ $profile['FIRST_NAME'] }}</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="form-group">
-                                    <label class="col-sm-4 control-label">{{ $data['LAST_NAME'] }}</label>
+                                    <label class="col-sm-4 control-label">{{ $profile['LAST_NAME'] }}</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="form-group">
-                                    <label class="col-sm-4 control-label">{{ $data['EMAIL'] }}</label>
+                                    <label class="col-sm-4 control-label">{{ $profile['EMAIL'] }}</label>
                                 </div>
                             </div>
                             <div class="row">
                                 <div class="form-group">
-                                    <label class="col-sm-4 control-label">{{ $data['TELEPHONE'] }}</label>
+                                    <label class="col-sm-4 control-label">{{ $profile['TELEPHONE'] }}</label>
                                 </div>
                             </div>
                             <a href="profile/edit">Edit Profile</a>
-                        @endforeach
                     </div>
                 </div>
             </div>
