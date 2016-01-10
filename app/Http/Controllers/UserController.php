cb16a1292e1255af453b08e02e835eb140998127
isojunior <isojocker@gmail.com>
2016-01-10 21:37:20 +0700

    Update เลือกคณะกับมหาลัย
    Update
    - เชื่อมต่อ ระหว่าง มหาลัยกับคณะ  กับ หน้า Profile

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -360,6 +360,16 @@ class UserController extends Controller {
 				'idUser' => $auth['ID_USER'],
 			],
 		]);
+		if ($updateFac["data"][0]["result"] == 1) {
+			$userResult = self::$factory->callWebservice([
+				'query' => [
+					'service' => 'getUser',
+					'idUser' => $auth['ID_USER'],
+				],
+			]);
+			Session::put('user', $userResult["data"][0]);
+			Session::flash('alert-success', 'Update Successful');
+		}
 		return redirect('profile');
 	}
 }
